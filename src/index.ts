72ef261552106a6dacb5e8847cd53f6e93a596b0
SebastianStovall <sebastianstovall@gmail.com>
2023-12-05 15:59:14 -0600

    valid anagram TS

--- src/index.ts
+++ src/index.ts
@@ -22,3 +22,37 @@ function containsDuplicate(nums: number[]): boolean {
 
 const nums = [0,4,5,0,3,6]
 console.log(containsDuplicate(nums))
+
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
+
+// https://leetcode.com/problems/valid-anagram/description/
+
+function isAnagram(s: string, t: string): boolean {
+    interface Tracker {
+        [props: string]: number
+    }
+    const tracker: Tracker = {}
+
+    if(s.length !== t.length) return false
+
+    for(let i = 0; i < s.length; i++) {
+        if(tracker[s[i]] === undefined) {
+            tracker[s[i]] = 1
+        } else {
+            tracker[s[i]]++
+        }
+    }
+
+    for(let i = 0; i < t.length; i++) {
+        if(tracker[t[i]] === undefined) {
+            return false
+        } else {
+            tracker[t[i]]--
+            if(tracker[t[i]] < 0) return false
+        }
+    }
+
+    return true
+
+};
