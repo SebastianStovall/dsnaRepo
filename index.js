1d8556b65a6500464c79d84cd5334d0e966dbd92
SebastianStovall <sebastianstovall@gmail.com>
2024-02-17 14:04:33 -0600

    compare triplits

--- index.js
+++ index.js
@@ -2717,3 +2717,25 @@ function findTotalHoursFromK(k, piles) {
     }
     return totalHours
 }
+
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
+
+// https://www.hackerrank.com/challenges/compare-the-triplets/problem?isFullScreen=true
+
+function compareTriplets(a, b) {
+    let alice = 0
+    let bob = 0
+
+    for(let i = 0; i < a.length; i++) {
+        if(a[i] > b[i]) {
+            alice++
+        } else if (a[i] < b[i]) {
+            bob++
+        } else if (a[i] === b[i]) {
+            // nothing
+        }
+    }
+
+    return [alice, bob]
+}
