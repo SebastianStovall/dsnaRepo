f6c683a838036f08af1c42ebd2c6d5a6b2a1c813
SebastianStovall <sebastianstovall@gmail.com>
2024-02-17 14:26:33 -0600

    plusMinus

--- index.js
+++ index.js
@@ -2753,3 +2753,28 @@ function aVeryBigSum(ar) {
     }
     return sum
 }
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
+
+// https://www.hackerrank.com/challenges/plus-minus/problem?isFullScreen=true
+
+function plusMinus(arr) {
+    let pos = 0;
+    let neg = 0;
+    let zero = 0;
+
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] > 0) {
+            pos++
+        } else if (arr[i] < 0) {
+            neg++
+        } else {
+            zero++
+        }
+    }
+
+    console.log( Number.parseFloat(pos / arr.length).toFixed(6) )
+    console.log( Number.parseFloat(neg / arr.length).toFixed(6) )
+    console.log( Number.parseFloat(zero / arr.length).toFixed(6) )
+
+}
