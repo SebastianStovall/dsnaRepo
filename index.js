0bb90bd7dfbbc4a706dd08d16e84fddfc046dc96
SebastianStovall <sebastianstovall@gmail.com>
2024-03-25 23:45:35 -0500

    find kth largest element in array

--- index.js
+++ index.js
@@ -3883,3 +3883,35 @@ var generate = function (numRows) {
   console.log("PASCAL ---> ", pascal);
   return pascal;
 };
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------- //
+
+var findKthLargest = function(nums, k) {
+  k = nums.length - k;
+
+ let quickSelect = function(l,r){
+     let pivot = nums[r]
+     let p = l
+
+     for(let i = l; i < r; i++){
+         if(nums[i] <= pivot){
+             [nums[i], nums[p]] = [nums[p], nums[i]]
+             p++
+         }
+     }
+
+     [nums[p], nums[r]] = [nums[r], nums[p]]
+
+     if(p > k){
+         return quickSelect(l, p-1)
+     } else if(p < k){
+         return quickSelect(p+1, r)
+     } else{
+         return nums[p]
+     }
+ }
+
+ return quickSelect(0, nums.length - 1)
+};
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------- //
