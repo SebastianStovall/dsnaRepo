87028ec2644ee6d76e4c7ff748eeb5d995ca3c78
SebastianStovall <sebastianstovall@gmail.com>
2024-04-20 14:39:49 -0500

    count good nodes

--- index.js
+++ index.js
@@ -4012,3 +4012,23 @@ var characterReplacement = function(s, k) {
 
 
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+function goodNodes(root) {
+  let count = 0;
+
+  const dfs = (node, maxSoFar) => {
+      if (!node) return;
+
+      if (node.val >= maxSoFar) {
+          count++;
+          maxSoFar = node.val;
+      }
+
+      dfs(node.left, maxSoFar);
+      dfs(node.right, maxSoFar);
+  };
+
+  dfs(root, -Infinity);
+  return count;
+}
